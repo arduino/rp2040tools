652929f155bef38c0b0f2a036a579fc599f8666b
Martino Facchin <m.facchin@arduino.cc>
2021-02-25 13:10:09 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module facchinm/rp2040load
+
+go 1.16
+
+require github.com/mattn/go-shellwords v1.0.11
---
+++ main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"time"
+)
+
+var (
+	verbose = flag.Bool("v", false, "Show verbose logging")
+	binary  = flag.String("D", "", "Path of the elf file to load")
+)
+
+const Version = "1.0.0"
+
+func PrintlnVerbose(a ...interface{}) {
+	if *verbose {
+		fmt.Println(a...)
+	}
+}
+
+func PrintVerbose(a ...interface{}) {
+	if *verbose {
+		fmt.Print(a...)
+	}
+}
+
+func main() {
+	name := filepath.Base(os.Args[0])
+	path, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	flag.Parse()
+
+	PrintlnVerbose(name + " " + Version + " - compiled with " + runtime.Version())
+
+	convert := []string{filepath.Join(path, "elf2uf2"), *binary, *binary + ".uf2"}
+	launchCommandAndWaitForOutput(convert, false, false)
+
+	info := []string{filepath.Join(path, "picotool"), "info"}
+	err, _, _ := launchCommandAndWaitForOutput(info, false, true)
+	for i := 0; i < 5 && err != nil; i++ {
+		err, _, _ = launchCommandAndWaitForOutput(info, false, true)
+		time.Sleep(500 * time.Millisecond)
+	}
+	if err != nil {
+		fmt.Println("")
+		os.Exit(1)
+	}
+
+	load := []string{filepath.Join(path, "picotool"), "load", *binary + ".uf2"}
+	err, _, _ = launchCommandAndWaitForOutput(load, true, false)
+	if err != nil {
+		fmt.Println("")
+		os.Exit(1)
+	}
+
+	reboot := []string{filepath.Join(path, "picotool"), "reboot"}
+	err, _, _ = launchCommandAndWaitForOutput(reboot, false, false)
+	if err != nil {
+		fmt.Println("")
+		os.Exit(1)
+	}
+
+	fmt.Println("")
+	os.Exit(0)
+}
+
+func launchCommandAndWaitForOutput(command []string, print_output bool, show_spinner bool) (error, bool, string) {
+	oscmd := exec.Command(command[0], command[1:]...)
+	tellCommandNotToSpawnShell(oscmd)
+	stdout, _ := oscmd.StdoutPipe()
+	stderr, _ := oscmd.StderrPipe()
+	multi := io.MultiReader(stdout, stderr)
+
+	if print_output && *verbose {
+		oscmd.Stdout = os.Stdout
+		oscmd.Stderr = os.Stderr
+	}
+	err := oscmd.Start()
+	in := bufio.NewScanner(multi)
+	in.Split(bufio.ScanRunes)
+	found := false
+	out := ""
+	if show_spinner {
+		fmt.Printf(".")
+	}
+	lastPrint := time.Now()
+	for in.Scan() {
+		if show_spinner && time.Since(lastPrint) > time.Second {
+			fmt.Printf(".")
+			lastPrint = time.Now()
+		}
+		out += in.Text()
+	}
+	err = oscmd.Wait()
+	return err, found, out
+}
+
+func launchCommandBackground(command []string) (error, bool) {
+	oscmd := exec.Command(command[0], command[1:]...)
+	tellCommandNotToSpawnShell(oscmd)
+	err := oscmd.Start()
+	return err, false
+}
