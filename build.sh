3d5199c315681ae3270c6ef5eb942b93fc7a2b98
umbynos <umberto.baldi@edu.unito.it>
2021-03-12 12:18:36 +0100

    success in building 🎉

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ export PATH=$PATH:/usr/local/Cellar/pkg-config/0.29.2/bin/
 
 apt install libfl-dev
 # rm -rf *
-export CROSS_COMPILE=x86_64-ubuntu12.04-linux-gnu
+export CROSS_COMPILE=x86_64-ubuntu16.04-linux-gnu
 cd deps/eudev-3.1.5
 export UDEV_DIR=`pwd`
 ./autogen.sh
@@ -50,9 +50,22 @@ mkdir build
 cd build
 #need to find a way to add the libudev.a
 cmake -DCMAKE_C_COMPILER=$CROSS_COMPILE-gcc -DCMAKE_CXX_COMPILER=$CROSS_COMPILE-g++ -DLIBUSB_LIBRARIES=$LIBUSB_DIR/libusb/.libs/libusb-1.0.a -DLIBUSB_INCLUDE_DIR=$LIBUSB_DIR/libusb/ ..
-make
+make VERBOSE=1 # allows to see the failing linking command: it misses libudev: after this run the link command adding it:
+/opt/x86_64-ubuntu16.04-linux-gnu-gcc/bin/x86_64-ubuntu16.04-linux-gnu-g++  -L/workdir/deps/eudev-3.1.5/src/libudev/.libs/  -L/workdir/deps/eudev-3.1.5/src/libudev/.libs/ $LIBUSB1_LIBS CMakeFiles/picotool.dir/main.cpp.o CMakeFiles/picotool.dir/picoboot_connection/picoboot_connection_cxx.cpp.o CMakeFiles/picotool.dir/picoboot_connection/picoboot_connection.c.o  -o picotool  /workdir/deps/libusb-1.0.20/libusb/.libs/libusb-1.0.a /workdir/deps/eudev-3.1.5/src/udev/.libs/libudev-core.a
+file picotool # picotool: ELF 64-bit LSB executable, x86-64, version 1 (SYSV), dynamically linked, interpreter /lib64/ld-linux-x86-64.so.2, for GNU/Linux 4.4.94, with debug_info, not stripped
+# TODO static link it with -static
+ldd picotool
+        # linux-vdso.so.1 (0x00007ffd6e5bf000)
+        # libpthread.so.0 => /lib/x86_64-linux-gnu/libpthread.so.0 (0x00007fa50a981000)
+        # libstdc++.so.6 => /lib/x86_64-linux-gnu/libstdc++.so.6 (0x00007fa50a7a0000)
+        # libm.so.6 => /lib/x86_64-linux-gnu/libm.so.6 (0x00007fa50a651000)
+        # libgcc_s.so.1 => /lib/x86_64-linux-gnu/libgcc_s.so.1 (0x00007fa50a636000)
+        # libc.so.6 => /lib/x86_64-linux-gnu/libc.so.6 (0x00007fa50a444000)
+        # /lib64/ld-linux-x86-64.so.2 (0x00007fa50a9ab000)
 cd ..
 cd ..
+
+
 
 cd pico-sdk/tools/elf2uf2/
 mkdir build
