e565d3f4db1e419edbece8f351b9d0b2a27a1af4
Martino Facchin <m.facchin@arduino.cc>
2022-04-20 15:05:35 +0200

    Add configure and make scripts for deps

--- docker/deps/build_libs.sh
+++ docker/deps/build_libs.sh
@@ -5,33 +5,74 @@ CROSS_COMPILER=${CROSS_COMPILE}-gcc
 else
 export CC=$CROSS_COMPILER
 fi
-# udev lib not required for macos
-if [[ ${CROSS_COMPILE} != *apple* ]]; then
-cd /opt/lib/eudev-3.2.10
-export UDEV_DIR=`pwd`
-./autogen.sh
-./configure --enable-static --disable-shared --disable-blkid --disable-kmod  --disable-manpages --host=${CROSS_COMPILE}
-make clean
-make -j4
-cd ..
-export CFLAGS="-I$UDEV_DIR/src/libudev/"
-export LDFLAGS="-L$UDEV_DIR/src/libudev/.libs/"
-export LIBS="-ludev"
-fi
 cd /opt/lib/libusb-1.0.20
 export LIBUSB_DIR=`pwd`
-./configure --enable-static --disable-shared --host=${CROSS_COMPILE}
-make clean
+./configure --prefix=${PREFIX} --disable-udev --enable-static --disable-shared --host=${CROSS_COMPILE}
 make
-cd ..
-# libusbudev.a merged not required for macos
-if [[ ${CROSS_COMPILE} != *apple* ]]; then
-mkdir -p ${CROSS_COMPILE}/libusb
-mkdir -p ${CROSS_COMPILE}/libudev
-cd ${CROSS_COMPILE}/libusb/
-ar -x ../../libusb-1.0.20/libusb/.libs/libusb-1.0.a
-cd ../libudev/
-ar -x ../../eudev-3.2.10/src/libudev/.libs/libudev.a
-cd ..
-ar -qc libusbudev.a libudev/* libusb/*
+make install
+
+if [[ $CROSS_COMPILE == "i686-w64-mingw32" ]] ; then
+  # libusb-compat is a mess to compile for win32
+  # use a precompiled version from libusb-win32 project
+  wget http://download.sourceforge.net/project/libusb-win32/libusb-win32-releases/1.2.6.0/libusb-win32-bin-1.2.6.0.zip
+  unzip libusb-win32-bin-1.2.6.0.zip
+  #mkdir -p $PREFIX/bin/
+  #cp libusb-win32-bin-1.2.6.0/bin/x86/libusb0_x86.dll $PREFIX/bin/libusb0.dll
+  cp libusb-win32-bin-1.2.6.0/include/lusb0_usb.h $PREFIX/include
+  cp libusb-win32-bin-1.2.6.0/lib/gcc/libusb.a $PREFIX/lib
+else
+
+cd /opt/lib/libusb-compat-0.1.5
+export LIBUSB0_DIR=`pwd`
+PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig" ./configure --prefix=${PREFIX} --enable-static --disable-shared --host=${CROSS_COMPILE}
+make
+make install
+
+cd /opt/lib/libelf-0.8.13
+export LIBELF_DIR=`pwd`
+./configure --disable-shared --host=$CROSS_COMPILE --prefix=${PREFIX}
+make
+make install
+
+cd /opt/lib/ncurses-5.9
+export NCURSES_DIR=`pwd`
+./configure --disable-shared --without-debug --without-ada --with-termlib --enable-termcap --host=$CROSS_COMPILE --prefix=${PREFIX}
+make
+make install.libs
+
+cd /opt/lib/readline-6.3
+export READLINE_DIR=`pwd`
+autoconf
+./configure --prefix=$PREFIX --disable-shared --host=$CROSS_COMPILE
+make
+make install-static
+
+cd /opt/lib/eudev-3.2.10
+./autogen.sh
+./configure --enable-static --disable-gudev --disable-introspection  --disable-shared --disable-blkid --disable-kmod  --disable-manpages --prefix=$PREFIX --host=${CROSS_COMPILE}
+make
+make install
+
+cd /opt/lib/hidapi
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
+#set +e
+#./bootstrap
+./bootstrap
+#set -e
+./configure --prefix=$PREFIX --enable-static --disable-shared --host=$CROSS_COMPILE
+make
+make install
+
+
+cd /opt/lib/libftdi1-1.4
+mkdir build
+
+CMAKE_EXTRA_FLAG="-DSHAREDLIBS=OFF -DBUILD_TESTS=OFF -DPYTHON_BINDINGS=OFF -DEXAMPLES=OFF -DFTDI_EEPROM=OFF"
+
+if [[ $CROSS_COMPILE == "i686-w64-mingw32" ]] ; then
+  CMAKE_EXTRA_FLAG="$CMAKE_EXTRA_FLAG -DCMAKE_TOOLCHAIN_FILE=./cmake/Toolchain-i686-w64-mingw32.cmake"
 fi
+
+cmake -DCMAKE_INSTALL_PREFIX="$PREFIX" -DLIBUSB_INCLUDE_DIR="$PREFIX/include/libusb-1.0" -DLIBFTDI_LIBRARY_DIRS="$PREFIX/lib" -DLIBUSB_LIBRARIES="usb-1.0" ../
+make
+make install
